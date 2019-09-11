a58280614db6674df9bc86a655e07eb8da6eef6c
sabbasachidas <sabbasachibd@gmail.com>
2019-09-11 22:47:45 +0600

    assignment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            string age = "";
+            string address = "";
+            string contact = "";
+            Console.WriteLine ("Enter ur name:");
+            name = Console.ReadLine();
+            Console.WriteLine ("Ur age:");
+            age = Console.ReadLine();
+            Console.WriteLine ("Ur address:");
+            address= Console.ReadLine();
+            Console.WriteLine ("Ur contact");
+            contact = Console.ReadLine();
+
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Ur age is " + age);
+            Console.WriteLine("U Lives at " + address);
+            Console.WriteLine("Ur contact no is  " + contact);
+            Console.ReadKey();
+        }
+    }
+}
